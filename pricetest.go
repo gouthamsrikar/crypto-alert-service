27bf7e52f39822e254424dfe3d28dac250e04526
gouthamsrikar <srikarsam20000@gmail.com>
2024-05-29 13:58:21 +0530

    print ma in while testing

--- pricetest.go
+++ pricetest.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"encoding/json"
+	"goalert/services"
 	"log"
 	"os"
 	"os/signal"
+	"strconv"
 
 	"github.com/gorilla/websocket"
 )
 
-// Message struct to represent the JSON message received from the WebSocket
 type Message struct {
 	EventType string  `json:"e"`
 	EventTime int64   `json:"E"`
@@ -25,20 +26,20 @@ func UnmarshalWelcome(data []byte) (Message, error) {
 }
 
 func main() {
-	// Define the endpoint for the Binance WebSocket API
 	endpoint := "wss://stream.binance.com:9443/ws"
 
-	// Define the symbols you want to subscribe to
-	symbols := []string{"btcusdt"} // Example symbols, you can change this to whatever you need
+	symbols := []string{"btcusdt"}
 
-	// Create a WebSocket connection
+	services.AddMovingAverageWindow("btcusdt", 5)
+	services.AddMovingAverageWindow("btcusdt", 10)
+	services.AddMovingAverageWindow("btcusdt", 15)
+
 	c, _, err := websocket.DefaultDialer.Dial(endpoint, nil)
 	if err != nil {
 		log.Fatal("dial:", err)
 	}
 	defer c.Close()
 
-	// Subscribe to the symbols
 	for _, symbol := range symbols {
 		subscribeMsg := []byte(`{"method":"SUBSCRIBE","params":["` + symbol + `@ticker"],"id":1}`)
 		err := c.WriteMessage(websocket.TextMessage, subscribeMsg)
@@ -47,7 +48,6 @@ func main() {
 		}
 	}
 
-	// Listen for messages
 	done := make(chan struct{})
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
@@ -71,7 +71,6 @@ func main() {
 		case <-interrupt:
 			log.Println("interrupt")
 
-			// Cleanly close the WebSocket connection by sending a close message and then waiting (with timeout) for the server to close the connection.
 			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
 			if err != nil {
 				log.Println("write close:", err)
@@ -85,7 +84,6 @@ func main() {
 	}
 }
 
-// prettyPrint function to print the JSON message in a human-readable format
 func prettyPrint(message []byte) {
 	var m Message
 	err := json.Unmarshal(message, &m)
@@ -93,7 +91,11 @@ func prettyPrint(message []byte) {
 		log.Println("unmarshal:", err)
 		return
 	}
-	// log.Printf(" Price: %s", m.Price)
+	if m.Price != "" {
+		floatPrice, _ := strconv.ParseFloat(m.Price, 64)
+		 services.AddPrice("btcusdt", floatPrice)
+		 services.PrintStockData("btcusdt")
+	}
 
 	log.Printf("Symbol: %s, Price: %s", m.Symbol, m.Price)
 }
